19b3dc386419df9571ea5c4eb492fc05e63f43f4
minzzun99 <skg06123@naver.com>
2026-04-15 16:24:31 +0900

    refactor: 상 기물 규칙 SangMoveRule 내부로 이동 및 board 내부 사용하지 않는 메서드 삭제

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -31,7 +31,7 @@ public class Board {
         }
 
         if (!(startPiece.canMovePosition(start, end) && startPiece.isDifferentCountry(endPiece.country())
-                && startPiece.isAvailableRoute(getSameLine(startPiece, start, end), endPiece.pieceType()))) {
+                && startPiece.isAvailableRoute(getPieceRoute(startPiece, start, end), endPiece.pieceType()))) {
             throw new IllegalArgumentException("말을 이동할 수 없습니다.");
         }
 
@@ -43,111 +43,14 @@ public class Board {
         return isEnd;
     }
 
-    private List<Piece> getSameLine(Piece startPiece, Position start, Position end) {
+    private List<Piece> getPieceRoute(Piece startPiece, Position start, Position end) {
         PieceType pieceType = startPiece.pieceType();
-        if (pieceType.equals(PieceType.MA)) {
-            return getMaRoute(start, end);
-        }
-        if (pieceType.equals(PieceType.SANG)) {
-            return getSangRoute(start, end);
-        }
-        if (pieceType.equals(PieceType.CHA) || pieceType.equals(PieceType.PO)) {
-            return getChaPoRoute(startPiece, start, end);
-        }
-
-        return Collections.emptyList();
-    }
-
-    private List<Piece> getChaPoRoute(Piece startPiece, Position start, Position end) {
-        if (start.getRow() == end.getRow() || start.getCol() == end.getCol()) {
-            return getStraightRoute(start, end);
-        }
-        return getDiagonalRoute(startPiece, start, end);
-    }
-
-    private List<Piece> getMaRoute(Position start, Position end) {
+        List<Position> piecePositions = pieceType.getRoutePosition(startPiece, start, end);
         List<Piece> pieces = new ArrayList<>();
-        int diffRow = end.getRow() - start.getRow();
-        int diffCol = end.getCol() - start.getCol();
 
-        Position position;
-        if (Math.abs(diffRow) == 2) {
-            position = Position.create(start.getRow() + (diffRow / 2), start.getCol());
-        } else {
-            position = Position.create(start.getRow(), start.getCol() + (diffCol / 2));
-        }
-
-        if (board.containsKey(position)) {
-            pieces.add(board.get(position));
-        }
-
-        return pieces;
-    }
-
-    private List<Piece> getSangRoute(Position start, Position end) {
-        int diffRow = end.getRow() - start.getRow();
-        int diffCol = end.getCol() - start.getCol();
-
-        Position position;
-        if (Math.abs(diffRow) == 3) {
-            position = Position.create(start.getRow() + (diffRow / 3 * 2), start.getCol() + (diffCol / 2));
-        } else {
-            position = Position.create(start.getRow() + (diffRow / 2), start.getCol() + (diffCol / 3 * 2));
-        }
-
-        List<Piece> pieces = getMaRoute(start, position);
-        if (board.containsKey(position)) {
-            pieces.add(board.get(position));
-        }
-
-        return pieces;
-    }
-
-    private List<Piece> getStraightRoute(Position start, Position end) {
-        if (start.getRow() == end.getRow()) {
-            return getHorizontalRoute(start, end);
-        }
-        return getVerticalRoute(start, end);
-    }
-
-    private List<Piece> getHorizontalRoute(Position start, Position end) {
-        List<Piece> pieces = new ArrayList<>();
-        int row = start.getRow();
-        int minCol = Math.min(start.getCol(), end.getCol());
-        int maxCol = Math.max(start.getCol(), end.getCol());
-        for (int i = minCol + 1; i < maxCol; i++) {
-            Position position = Position.create(row, i);
+        for (Position position : piecePositions) {
             if (board.containsKey(position)) {
                 pieces.add(board.get(position));
-            }
-        }
-
-        return pieces;
-    }
-
-    private List<Piece> getVerticalRoute(Position start, Position end) {
-        List<Piece> pieces = new ArrayList<>();
-        int col = start.getCol();
-        int minRow = Math.min(start.getRow(), end.getRow());
-        int maxRow = Math.max(start.getRow(), end.getRow());
-        for (int i = minRow + 1; i < maxRow; i++) {
-            Position position = Position.create(i, col);
-            if (board.containsKey(position)) {
-                pieces.add(board.get(position));
-            }
-        }
-
-        return pieces;
-    }
-
-    private List<Piece> getDiagonalRoute(Piece startPiece, Position start, Position end) {
-        List<Piece> pieces = new ArrayList<>();
-        Palace palace = Palace.from(startPiece.country());
-
-        if (palace.isDiagonalPath(start, end) && Math.abs(end.getRow() - start.getRow()) == 2) {
-            Position center = palace.getCenter();
-            if (board.containsKey(center)) {
-                pieces.add(board.get(center));
             }
         }
 
--- src/main/java/domain/strategy/SangMoveRule.java
+++ src/main/java/domain/strategy/SangMoveRule.java
@@ -3,6 +3,7 @@ package domain.strategy;
 import domain.Position;
 import domain.Piece;
 import domain.constant.PieceType;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SangMoveRule implements MoveRule {
@@ -18,5 +19,37 @@ public class SangMoveRule implements MoveRule {
     @Override
     public boolean isAvailableRoute(List<Piece> pieces, PieceType endPieceType) {
         return pieces.isEmpty();
+    }
+
+    @Override
+    public List<Position> getRoutePosition(Piece startPiece, Position start, Position end) {
+        int diffRow = end.getRow() - start.getRow();
+        int diffCol = end.getCol() - start.getCol();
+
+        Position position;
+        if (Math.abs(diffRow) == 3) {
+            position = Position.create(start.getRow() + (diffRow / 3 * 2), start.getCol() + (diffCol / 2));
+        } else {
+            position = Position.create(start.getRow() + (diffRow / 2), start.getCol() + (diffCol / 3 * 2));
+        }
+
+        List<Position> positions = getMaRoutePosition(start, position);
+        positions.add(position);
+
+        return positions;
+    }
+
+    public List<Position> getMaRoutePosition(Position start, Position end) {
+        List<Position> positions = new ArrayList<>();
+        int diffRow = end.getRow() - start.getRow();
+        int diffCol = end.getCol() - start.getCol();
+
+        if (Math.abs(diffRow) == 2) {
+            positions.add(Position.create(start.getRow() + (diffRow / 2), start.getCol()));
+        } else {
+            positions.add(Position.create(start.getRow(), start.getCol() + (diffCol / 2)));
+        }
+
+        return positions;
     }
 }
